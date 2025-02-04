08aa8ee7dca7306f233abfcbb0b6c0ae2875b05b
detail.wes <details.wes@gmail.com>
2025-02-04 18:55:07 +0530

    worked on configuration form

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -1,27 +1,60 @@
 import {
   Page,
   Layout,
-  Text,
   Card,
-  BlockStack,
+  Loading,
+  Frame,
+  Text,
+  BlockStack
 } from "@shopify/polaris";
+import { useNavigation } from "@remix-run/react";
+
 export default function Index() {
-  console.log("testing")  
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
+  if (isLoading) {
+    return (
+      <Frame>
+        <Page>
+          <Layout>
+            <Layout.Section>
+              <Card>
+                <div style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  padding: "40px"
+                }}>
+                  <BlockStack gap="200">
+                    <Text variant="bodyMd" as="p" alignment="center">
+                     Please Wait...
+                    </Text>
+                  </BlockStack>
+                  <Loading />
+                </div>
+              </Card>
+            </Layout.Section>
+          </Layout> 
+        </Page>
+      </Frame>
+    );
+  }
   return (
-    <Page>
-      <BlockStack gap="500">
+    <Frame>
+      <Page>
         <Layout>
           <Layout.Section>
             <Card>
-                <BlockStack gap="200">
-                  <Text as="h2" variant="headingMd">
-                    Admin configuration 
-                  </Text>
-                </BlockStack>
+              <BlockStack gap="200">
+                <Text as="h4" variant="headingMd">
+                  Admin configuration
+                </Text>
+              </BlockStack>
             </Card>
           </Layout.Section>
         </Layout>
-      </BlockStack>
-    </Page>
+      </Page>
+    </Frame>
   );
 }
--- app/routes/app.allProducts.tsx
+++ app/routes/app.allProducts.tsx
@@ -106,6 +106,10 @@ export async function loader({ request }: LoaderFunctionArgs) {
     })
     console.log("dbShapaSizeRaw", dbShapaSizeRaw)
 
+
+
+
+
     // Transform the data to match the expected types
     const dbImages: DBImage[] = dbImagesRaw.map(img => ({
       id: img.id,
@@ -125,7 +129,9 @@ export async function loader({ request }: LoaderFunctionArgs) {
       width: shape.width ? new Decimal(shape.width) : null,
       image: shape.image
     }))
-    
+
+
+
     return json({
       products: productsData.data.products.edges,
       dbImages,
@@ -365,7 +371,7 @@ export default function ProductsPage() {
                 }}>
                   <Loading />
                   <Text variant="bodyMd" as="p" alignment="center">
-                    Loading products...
+                  Please wait...
                   </Text>
                 </div>
               </Card>
@@ -374,7 +380,7 @@ export default function ProductsPage() {
         </Page>
       </Frame>
     );
-  }  //6
+  }  //7
 
   const resourceName: ResourceListProps["resourceName"] = {
     singular: "product",
