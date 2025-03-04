71192d38512089698aadf78b9e5ce831b87997e9
detailswes <details.wes@gmail.com>
2025-03-04 19:05:02 +0530

    Updated the checkmark code

--- extensions/storefront-product-customizer/assets/product-customizer.js
+++ extensions/storefront-product-customizer/assets/product-customizer.js
@@ -179,11 +179,9 @@ document.addEventListener("DOMContentLoaded", async () => {
                class="shapes-sizes" 
                style="width: 100px; height: 75px; cursor: pointer; object-fit: contain; border:1px solid black; border-radius: 0px;">
           <p style="text-align:center;margin:0px">${shape.width}" * ${shape.height}"</p>
-          <div class="checkmark ${index === 0 ? "active" : ""}" style="position: absolute; top: -5px; right: -5px; width: 20px; height: 20px; background-color: #4CAF50; border-radius: 50%; display: flex;
-          <!--${
+          <div class="checkmark ${index === 0 ? "active" : ""}" style="position: absolute; top: -5px; right: -5px; width: 20px; height: 20px; background-color: #4CAF50; border-radius: 50%; display: ${
             index === 0 ? "flex" : "none"
-          }; -->
-          align-items: center; justify-content: center;">
+          }; align-items: center; justify-content: center;">
             <span style="color: white; font-size: 14px;">✓</span>
           </div>
         </div>`,
